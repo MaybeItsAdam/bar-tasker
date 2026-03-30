9af8be85798abda9451d15fc7ee68ec346e7b378
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2026-03-30 18:17:28 +0100

    chore: rename core package id and refresh app icons
    - rename Swift package from bar-tasker-corelogic to bar-tasker-core
    
    - regenerate all AppIcon.appiconset sizes from the new 1024 source icon

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "bar-tasker-corelogic",
+  name: "bar-tasker-core",
   platforms: [.macOS(.v15)],
   products: [
     .library(name: "BarTaskerCore", targets: ["BarTaskerCore"])
